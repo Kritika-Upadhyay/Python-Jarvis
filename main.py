304e533d9cb34115a46a9a83d94ed55cf2f7f790
Kritika Upadhyay <kritika1532oo8@gmail.com>
2026-08-11 15:44:45 +0530

    Implement voice recognition and Jarvis wake-word detection

--- main.py
+++ main.py
@@ -10,41 +10,68 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processCommand(c):
+    pass
+
+def listen():
+    sample_rate = 16000
+    duration = 5
+            
+    recording = sd.rec(
+        int(duration * sample_rate),
+        samplerate = sample_rate,
+        channels = 1,
+        dtype = "int16"
+    )
+            
+    sd.wait()
+            
+    return sr.AudioData(
+        recording.tobytes(),
+        sample_rate,
+        2
+    )
+
 if __name__ == "__main__":
     speak("Initializing Jarvis...")
 
     while True: 
         print("Listening...")
 
-        # Record audio from microphone
-        sample_rate = 16000
-        duration = 5
-
-        recording = sd.rec(
-            int(duration * sample_rate),
-            samplerate = sample_rate,
-            channels = 1,
-            dtype = "int16"
-        )
-
-        sd.wait()
-
-        # Convert recording to SpeechRecognition AudioData
-        audio = sr.AudioData(
-            recording.tobytes(),
-            sample_rate,
-            2
-        )
-
-        print("Recognizing...")
-
         try:
-            text = recognizer.recognize_google(audio)
-            print("Google thinks you said: " + text)
+            
+            # Listen for wake word
+            audio = listen()
+            word = recognizer.recognize_google(audio)
 
-        except sr.UnknownValueError:
-            print("Google could not understand audio")
+            if word.lower() == "jarvis":
+                print(word)
+                speak("Yeah")
 
-        except sr.RequestError as e:
-            print(f"Google error; {e}")
+                # Listen for command
+                print("Jarvis Active...")
+
+                audio = listen()
+                command = recognizer.recognize_google(audio)
+                print(command)
+
+                processCommand(command)
+
+        except Exception as e:
+            print(f"Error; {e}")
         
+
+# try:
+#     with sr.Microphone() as source:
+#         print("Listening...")
+#         audio = r.listen(source, timeout=2, phrase_time_limit=1)
+#     word = r.recognize_google(audio)
+#     if(word.lower() == "jarvis"):
+#         speak("Yeah")
+#         # Listen for command
+#         with sr.Microphone() as source:
+#             print("Jarvis Active...")
+#             audio = r.listen(source)
+#             command = r.recognize_google(audio)
+
+#             processCommand()
